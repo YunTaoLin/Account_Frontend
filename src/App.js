98ef7d8fe0009160d9b5bd19098bc9169bf946ab
YunTaoLin <404200173@gapp.fju.edu.tw>
2022-07-04 18:17:45 +0800

    新增主動登入按鈕

--- src/App.js
+++ src/App.js
@@ -1,92 +1,106 @@
 import "./App.scss";
-import React,{useEffect} from "react";
-import {  Route,Switch,Redirect ,useLocation } from "react-router-dom";
-import 'swiper/swiper.scss';
+import React, { useEffect, useState } from "react";
+import { Route, Switch, Redirect, useLocation } from "react-router-dom";
+import "swiper/swiper.scss";
 import { connect } from "react-redux";
-import {getAccount} from './redux/account/action';
-import {getMember} from './redux/member/action';
-import {Loading} from './redux/loading/action';
+import { getAccount } from "./redux/account/action";
+import { getMember } from "./redux/member/action";
+import { Loading } from "./redux/loading/action";
 //自定組件
-import Navbar from './components/navbar';
-import Loadging from './components/Loading'
-import ApiWait from './components/ApiWait'
-import Drawer from './components/drawer';
-import Account from './page/Account';
-import Chart from './page/Chart';
-import Profile from './page/Profile';
-import Add from './page/Add';
-import SetBudget from './page/SetBudget';
-import Connect from './page/Connect';
+import Navbar from "./components/navbar";
+import Loadging from "./components/Loading";
+import ApiWait from "./components/ApiWait";
+import Drawer from "./components/drawer";
+import Account from "./page/Account";
+import Chart from "./page/Chart";
+import Profile from "./page/Profile";
+import Add from "./page/Add";
+import SetBudget from "./page/SetBudget";
+import Connect from "./page/Connect";
 //API
-import {POST_Token,Line_LoginAuth,LINE_DATA,POST_CreateMember} from './JS/api';
-import Qs from 'qs'
-import cookie from 'react-cookies'
+import {
+  POST_Token,
+  Line_LoginAuth,
+  LINE_DATA,
+  POST_CreateMember,
+} from "./JS/api";
+import Qs from "qs";
+import cookie from "react-cookies";
 
-const useQuery=()=>{
+const useQuery = () => {
   return new URLSearchParams(useLocation().search);
-}
+};
+const toLineLogin = () => {
+  window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.REACT_APP_client_id}&redirect_uri=${process.env.REACT_APP_RETURN_URL}&state=123&scope=profile%20openid%20email`;
+};
 
 function APP(props) {
-  let query = useQuery()
+  let query = useQuery();
+  const [afterLogin, setAfterLogin] = useState(false);
   useEffect(
     () => {
       //  如果cookie沒有UID，則導入登入頁面
-       if(cookie.load('UID')){
-        myLogin()
-      }else{
-         //如果網址有callback，則呼叫LINE解析，取得UID
-        if(window.location.href.indexOf("code") > -1){
+      if (cookie.load("UID")) {
+        setAfterLogin(true);
+        myLogin();
+      } else {
+        //如果網址有callback，則呼叫LINE解析，取得UID
+        if (window.location.href.indexOf("code") > -1) {
           let lineData = {
             grant_type: "authorization_code",
             redirect_uri: process.env.REACT_APP_RETURN_URL,
             client_id: process.env.REACT_APP_client_id,
             client_secret: process.env.REACT_APP_secret,
-            code: query.get('code'),
+            code: query.get("code"),
           };
           let passData = Qs.stringify(lineData);
           Line_LoginAuth(passData)
-          .then(({ data }) => {
-            LINE_DATA(data.access_token).then((LineData) => {
-              cookie.save('UID',LineData.data.userId,{path:"/"})
-              cookie.save('name',LineData.data.displayName,{path:"/"})
-              cookie.save('headimg',LineData.data.pictureUrl,{path:"/"})
-              myLogin()
+            .then(({ data }) => {
+              LINE_DATA(data.access_token).then((LineData) => {
+                cookie.save("UID", LineData.data.userId, { path: "/" });
+                cookie.save("name", LineData.data.displayName, { path: "/" });
+                cookie.save("headimg", LineData.data.pictureUrl, { path: "/" });
+                setAfterLogin(true);
+                myLogin();
+              });
+            })
+            .catch((error) => {
+              alert(`開始失敗${error}`);
+              toLineLogin();
             });
-          })
-          .catch((error) => {
-            alert(`開始失敗${error}`)
-            window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.REACT_APP_client_id}&redirect_uri=${process.env.REACT_APP_RETURN_URL}&state=123&scope=profile%20openid%20email`
-          });
-        }else{
-          window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.REACT_APP_client_id}&redirect_uri=${process.env.REACT_APP_RETURN_URL}&state=123&scope=profile%20openid%20email`
         }
       }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [],
+    []
   );
-  const myLogin = ()=>{
-    POST_Token(cookie.load('UID')).then(({data})=>{
-      if(data.status==='Y'){
-        cookie.save('token',data.token,{path:"/"})
-        props.getAccount()
-        props.getMember()
-      }else{
-        //進行註冊
-        POST_CreateMember({
-          "uid": cookie.load('UID'),
-          "name": cookie.load('name'),
-          "budget": 0,
-          "creDate": new Date()
-        })
-        .then(setTimeout(()=>myLogin(),100))
-      }
-    }).catch(()=>{
-    window.location.href = `https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${process.env.REACT_APP_client_id}&redirect_uri=${process.env.REACT_APP_RETURN_URL}&state=123&scope=profile%20openid%20email`})
-  }
+  const myLogin = () => {
+    POST_Token(cookie.load("UID"))
+      .then(({ data }) => {
+        if (data.status === "Y") {
+          cookie.save("token", data.token, { path: "/" });
+          props.getAccount();
+          props.getMember();
+        } else {
+          //進行註冊
+          POST_CreateMember({
+            uid: cookie.load("UID"),
+            name: cookie.load("name"),
+            budget: 0,
+            creDate: new Date(),
+          }).then(setTimeout(() => myLogin(), 100));
+        }
+      })
+      .catch(() => {
+        toLineLogin();
+      });
+  };
   return (
-    <div className="mobile_container" style={{height:window.innerHeight - 56}}>
-      <Drawer style={{position:'relative'}}></Drawer>
+    <div
+      className="mobile_container"
+      style={{ height: window.innerHeight - 56 }}
+    >
+      <Drawer style={{ position: "relative" }}></Drawer>
       <Switch>
         <Route path="/Add" component={Add}></Route>
         <Route path="/Chart" component={Chart}></Route>
@@ -97,17 +111,14 @@ function APP(props) {
         <Redirect to="/Account" />
       </Switch>
       <Navbar></Navbar>
-      <Loadging />
-      {props.data.loadingStatus.apiwait && <ApiWait/>} 
+      <Loadging afterLogin={afterLogin} toLineLogin={toLineLogin} />
+      {props.data.loadingStatus.apiwait && <ApiWait />}
     </div>
   );
 }
 
-export default connect (
-  state => ({data: state }), 
-  {
-    getAccount,
-    getMember,
-    Loading
-  }
-)(APP);
+export default connect((state) => ({ data: state }), {
+  getAccount,
+  getMember,
+  Loading,
+})(APP);
--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,53 +1,78 @@
-import React from 'react';
-import styled from 'styled-components'
-import title from '../../asset/title.png'
-import loadingimg from '../../asset/Thin fading line.gif'
+import React from "react";
+import styled from "styled-components";
+import title from "../../asset/title.png";
+import loadingimg from "../../asset/Thin fading line.gif";
+import LineLoginImg from "../../asset/login_line.png";
 import { connect } from "react-redux";
 const Index = (props) => {
-    return (
-        <Loading style={{opacity:( props.data.loadingStatus.loading?1:0),
-            visibility:( props.data.loadingStatus.loading?'visible':'hidden')
-          }}>
-            <div className="title">
-                <img src={title} alt=""/>   
-            </div>
-            <div className="loading">
-                <img src={loadingimg} alt=""/>
-            </div>
-        </Loading>
-    )
-}
+  return (
+    <Loading
+      style={{
+        opacity: props.data.loadingStatus.loading ? 1 : 0,
+        visibility: props.data.loadingStatus.loading ? "visible" : "hidden",
+      }}
+    >
+      <div className="title">
+        <img src={title} alt="" />
+      </div>
+      {props.afterLogin ? (
+        <div className="loading">
+          <img src={loadingimg} alt="" />
+        </div>
+      ) : (
+        <button className="login__btn mt_12" onClick={props.toLineLogin}>
+          <img src={LineLoginImg} alt="Line登入" />
+          <div>使用 LINE 登入</div>
+        </button>
+      )}
+    </Loading>
+  );
+};
 
-export default connect (
-    state => ({data: state }), 
-  )(Index);
+export default connect((state) => ({ data: state }))(Index);
 
 const Loading = styled.div`
-    width:100vw;
-    height:100vh;
-    background-color: #fefefe;
-    position: fixed;
-    top:0;
-    left:0;
-    right:0;
-    bottom:0;
-    z-index:99999999;
-    display:flex;
-    flex-direction:column;
-    justify-content:center;
-    align-items:center;
-    transition:.3s;
-    .title{
-        width:70%;
-        img{
-            width:100%;
-        }
+  width: 100vw;
+  height: 100vh;
+  background-color: #fefefe;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99999999;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: 0.3s;
+  .title {
+    width: 70%;
+    img {
+      width: 100%;
     }
-    .loading{
-        width:60%;
-        margin-bottom:10vh;
-        img{
-            width:100%;
-        }
+  }
+  .loading {
+    width: 60%;
+    margin-bottom: 10vh;
+    img {
+      width: 100%;
     }
-`
+  }
+  .login__btn {
+    width: 80%;
+    height: 50px;
+    font-size: 16px;
+    margin-bottom: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #00c300;
+    color: #fff;
+    position: relative;
+    img {
+      position: absolute;
+      left: 20px;
+    }
+  }
+`;
